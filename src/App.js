20dd6072fde8be440297e33b942adf3a331c5c8b
Ultronoz <diegonadalettiult@gmail.com>
2021-07-25 23:21:03 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
+function req() {
+  fetch("https://api.adviceslip.com/advice")
+  .then(function(res) {
+    return res.json()
+  })
+  .then(function(data) {
+  
+    document.getElementById('h1').innerHTML = data.slip.advice;
+  })
+  
+}
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        <div class="p-5 " id="jumbo">
+          <center>
+            <h1 class="display-3">Precisa de uma conselho?</h1>
+            <p class="lead">Aperte o botão!</p>
+            
+              <Button onClick={req}>Clique aqui!</Button>
+            
+            </center>
+        </div>
+        <div class="container-fluid">
+          <center>
+            <h1 id="h1"></h1>
+          </center>
+        </div>
+      
+      
       </header>
     </div>
   );
